5e1d322629774a84e437b1f96de429697a1f5d4d
Gromovoi Stanislav <gromila12@rambler.ru>
2023-03-22 14:54:29 +0600

    Create "AuthService"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from "./user/user.module";
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -20,7 +21,8 @@ import { UserModule } from "./user/user.module";
       autoLoadEntities: true,
       synchronize: true
     }),
-    UserModule
+    UserModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { from, Observable } from "rxjs";
+
+@Injectable()
+export class AuthService {
+
+  hashPassword(password: string): Observable<string> {
+    return from(bcrypt.hash(password, 12));
+  }
+
+  comparePasswords(password: string, storedPasswordHash: string): Observable<any> {
+    return from(bcrypt.compare(password, storedPasswordHash));
+  }
+
+}
